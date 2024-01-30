30e3bc139c86c4ed3c1c56f13bd3991d8a2d95e8
madimetja francis <fmvundlela@gmail.com>
2024-01-30 07:41:18 +0200

    navbar functioning

--- public/script.js
+++ public/script.js
@@ -1,3 +1,8 @@
+document.getElementById("contact").scrollIntoView({ behavior: "smooth" });
+document.getElementById("skills").scrollIntoView({ behavior: "smooth" });
+document.getElementById("projects").scrollIntoView({ behavior: "smooth" });
+document.getElementById("about").scrollIntoView({ behavior: "smooth" });
+
 document
   .getElementById("contact-form")
   .addEventListener("submit", function (event) {
@@ -7,18 +12,19 @@ document
     const email = document.getElementById("email").value;
     const message = document.getElementById("message").value;
 
-    fetch("/api/contacts/add", {method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ names, email, message }),
+    fetch("/api/contacts/add", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ names, email, message }),
     })
-    .then(response => response.text())
-    .then(data => {
+      .then((response) => response.text())
+      .then((data) => {
         alert(data);
-        document.getElementById('contact-form').reset();
-    })
-    .catch((error) => {
-        console.error('Error:', error);
-    });
-});
+        document.getElementById("contact-form").reset();
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  });
