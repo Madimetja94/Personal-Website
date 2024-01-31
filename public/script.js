9b8d4f9ba1954f31280f494b775d4ee6cf522aff
madimetja francis <fmvundlela@gmail.com>
2024-02-01 00:08:36 +0200

    hamburger menu

--- public/script.js
+++ public/script.js
@@ -1,3 +1,29 @@
+const menuClose = document.getElementById("menu-close");
+const menuOpen = document.getElementById("menu-open");
+
+const navLinks = document.getElementById("navbar-links");
+menuOpen.classList.add("open");
+menuClose.classList.add("close");
+navLinks.classList.add("close");
+
+menuOpen.onclick = () => {
+  menuOpen.classList.remove("open");
+  menuOpen.classList.add("close");
+  menuClose.classList.remove("close");
+  menuClose.classList.add("open");
+  navLinks.classList.remove("close");
+  navLinks.classList.add("open");
+};
+
+menuClose.onclick = () => {
+  menuOpen.classList.remove("close");
+  menuOpen.classList.add("open");
+  menuClose.classList.remove("open");
+  menuClose.classList.add("close");
+  navLinks.classList.remove("open");
+  navLinks.classList.add("close");
+};
+
 document
   .getElementById("contact-form")
   .addEventListener("submit", function (event) {
