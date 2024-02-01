7ea642113d2695958e710b3e4d0028ec0993340e
Madimetja Movundlela <Madimetja.Movundlela@bbd.co.za>
2024-02-01 16:18:58 +0200

    favicon error removed

--- src/config/config.js
+++ src/config/config.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 const Pool = require("pg").Pool;
 const pool = new Pool({
-  user: process.env.POSTGRES_USER,
-  host: process.env.HOST,
-  database: process.env.POSTGRES_DB,
-  password: process.env.POSTGRES_PASSWORD,
-  port: process.env.POSTGRES_PORT,
+  user: process.env.POSTGRES_USER || "postgres",
+  host: process.env.HOST || "localhost",
+  database: process.env.POSTGRES_DB || "postgres",
+  password:"1234567",
+  port: process.env.POSTGRES_PORT|| 5432,
 });
 module.exports = { pool };
